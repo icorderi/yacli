2dd6fb5e82b4a4409e22f28d0748833d348a20d9
Ignacio Corderi <icorderi@msn.com>
2016-02-11 15:13:16 -0800

    fix: Updated token for Travis

--- .travis-update-gh-pages.sh
+++ .travis-update-gh-pages.sh
@@ -1,14 +1,17 @@
+#!/bin/bash
+
 # based on: http://sleepycoders.blogspot.se/2013/03/sharing-travis-ci-generated-files.html
+# and: https://gist.github.com/domenic/ec8b0fc8ab45f39403dd
+
+set -e # exit with nonzero exit code if anything fails
 
 # Only do it if not acting on a pull request.
 if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
-  GH_REPO="github.com/icorderi/yacli.git"
-
   # Go to home and setup git
   cd $HOME
+  git config --global user.name "Travis CI"
   git config --global user.email "travis@travis-ci.org"
-  git config --global user.name "Travis"
 
   # Using token clone gh-pages branch. Pipe to /dev/null to avoid printing the decrypted key.
   git clone --quiet --branch=gh-pages https://${GH_TOKEN}@${GH_REPO}  gh-pages > /dev/null 2>&1
@@ -21,6 +24,6 @@ if [ "$TRAVIS_PULL_REQUEST" == "false" ]; then
 
   # add, commit and push files
   git add --all -f .
-  git commit -m "Travis build $TRAVIS_BUILD_NUMBER pushed to gh-pages"
+  git commit -m "Travis build $TRAVIS_BUILD_NUMBER docs pushed to gh-pages"
   git push -fq origin gh-pages > /dev/null 2>&1
 fi
