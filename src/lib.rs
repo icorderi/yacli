a3054b0ed858faabcd187852e492666f555ffc08
Ignacio Corderi <icorderi@msn.com>
2016-06-29 13:28:59 -0700

    feat: add main for cli command

--- src/lib.rs
+++ src/lib.rs
@@ -1,40 +1,21 @@
-// Copyright (c) 2015 Ignacio Corderi
-
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
-// author: Ignacio Corderi
-
 extern crate shell;
 extern crate docopt;
 extern crate rustc_serialize;
 
 pub use ::shell::MultiShell; // re-export
-use ::docopt::Docopt;
-use ::rustc_serialize::Decodable;
+
 use ::std::env;
 use ::std::error::Error;
 use ::std::marker::PhantomData;
 
-pub trait CliCommand<E> : Sized + Decodable {
+use ::docopt::Docopt;
+use ::rustc_serialize::Decodable;
+
+/// Represents CLI commands
+pub trait CliCommand<E>: Decodable {
     fn from_argv(argv: Vec<String>) -> Self {
         ::docopt::Docopt::new(Self::usage())
-            .and_then(|d| d.argv(argv.clone().into_iter()).decode() )
+            .and_then(|d| d.argv(argv.clone().into_iter()).decode())
             .unwrap_or_else(|e| e.exit())
     }
 
@@ -43,20 +24,28 @@ pub trait CliCommand<E> : Sized + Decodable {
     fn usage() -> &'static str;
 }
 
+/// Represents a CLI command dispatcher
 pub trait CliDispatcher<E> {
     fn dispatch(&self, Vec<String>, &mut MultiShell) -> Result<(), E>;
 }
 
-pub trait CliArgs<E, D>: Sized + Decodable
-where D: CliDispatcher<E>
+/// Represents a cli args
+pub trait CliArgs<E, D>: Decodable
+    where D: CliDispatcher<E>
 {
     fn usage() -> &'static str;
 
-    fn get_verbose(&self) -> bool { false }
+    fn get_verbose(&self) -> bool {
+        false
+    }
 
-    fn get_show_list(&self) -> bool { false }
+    fn get_show_list(&self) -> bool {
+        false
+    }
 
-    fn get_list(&self) -> Vec<String> { vec![] }
+    fn get_list(&self) -> Vec<String> {
+        vec![]
+    }
 
     fn get_dispatcher<'a>(&'a self) -> &'a Option<D>;
 
@@ -65,7 +54,7 @@ where D: CliDispatcher<E>
     fn try_dispatch(&self, shell: &mut MultiShell) -> Result<(), E> {
         match *self.get_dispatcher() {
             Some(ref cmd) => try!(cmd.dispatch(self.get_args().clone(), shell)),
-            None =>  println!("{}", Self::usage()),
+            None => println!("{}", Self::usage()),
         };
         Ok(())
     }
@@ -81,47 +70,50 @@ pub struct GenericArgs<C> {
 
 impl<C: Decodable> GenericArgs<C> {
     pub fn from_argv(usage: &str, argv: Vec<String>) -> Self {
-        let docopt = Docopt::new(usage).unwrap()
-                            .options_first(true)
-                            .argv(argv.iter().map(|s| &s[..]))
-                            .help(true);
+        let docopt = Docopt::new(usage)
+            .unwrap()
+            .options_first(true)
+            .argv(argv.iter().map(|s| &s[..]))
+            .help(true);
 
         docopt.decode().unwrap_or_else(|e| e.exit())
     }
 }
 
-pub struct CliApplication<E, D, A>
-{
-    _error_type: PhantomData<E>,      // Marker
+pub struct CliApplication<E, D, A> {
+    _error_type: PhantomData<E>, // Marker
     _dispatcher_type: PhantomData<D>, // Marker
-    _args_type: PhantomData<A>,       // Marker
+    _args_type: PhantomData<A>, // Marker
     usage: String,
     version: Option<String>,
 }
 
 impl<E, D, A> CliApplication<E, D, A>
-where E: Error,
-      D: CliDispatcher<E>,
-      A: CliArgs<E, D> + Decodable
+    where E: Error,
+          D: CliDispatcher<E>,
+          A: CliArgs<E, D> + Decodable
 {
-
     pub fn new<U: Into<String>, V: Into<String>>(usage: U, version: Option<V>) -> Self {
         let v = match version {
-                    Some(v) => Some(v.into()),
-                    None    => None,
-                };
-         CliApplication { usage: usage.into(), version: v,
-                          _error_type: PhantomData,
-                          _dispatcher_type: PhantomData,
-                          _args_type: PhantomData }
+            Some(v) => Some(v.into()),
+            None => None,
+        };
+        CliApplication {
+            usage: usage.into(),
+            version: v,
+            _error_type: PhantomData,
+            _dispatcher_type: PhantomData,
+            _args_type: PhantomData,
+        }
     }
 
-    fn main_with_args(&self, args: &[String], shell: &mut MultiShell) -> Result<(),E> {
-        let docopt = Docopt::new(self.usage.clone()).unwrap()
-                            .options_first(true)
-                            .argv(args.iter().map(|s| &s[..]))
-                            .help(true)
-                            .version(self.version.clone());
+    fn main_with_args(&self, args: &[String], shell: &mut MultiShell) -> Result<(), E> {
+        let docopt = Docopt::new(self.usage.clone())
+            .unwrap()
+            .options_first(true)
+            .argv(args.iter().map(|s| &s[..]))
+            .help(true)
+            .version(self.version.clone());
 
         let args: A = docopt.decode().unwrap_or_else(|e| e.exit());
         shell.set_verbose(args.get_verbose());
@@ -139,7 +131,7 @@ where E: Error,
             None => {
                 println!("{}", &self.usage);
                 Ok(())
-            },
+            }
         }
     }
 
@@ -151,5 +143,16 @@ where E: Error,
             Ok(_) => (),
             Err(e) => shell.error_full(&e, true).unwrap(),
         }
+    }
+}
+
+pub fn main<C: CliCommand<E>, E: Error>() {
+    let mut shell = MultiShell::new_stdio(false);
+    let args: Vec<_> = env::args().collect();
+    let cmd = C::from_argv(args);
+    let r = cmd.execute(&mut shell);
+    match r {
+        Ok(_) => (),
+        Err(e) => shell.error_full(&e, true).unwrap(),
     }
 }
